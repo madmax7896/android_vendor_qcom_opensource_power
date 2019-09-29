bc158b6588bbcdf64aac73f146b0dcf880f1fff5
Michael Bestas <mkbestas@lineageos.org>
2019-09-29 21:06:22 +0300

    power: Add -qti suffix to module name
    * Prevent conflict with other HALs
    
    Change-Id: Icf1216410db26c3bb811d7aedde03e6d2f87e3cc
    Signed-off-by: TH779 <tongsj2333@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- Android.mk
+++ Android.mk
@@ -120,8 +120,8 @@ LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 else
-LOCAL_MODULE := android.hardware.power@1.2-service
-LOCAL_INIT_RC := android.hardware.power@1.2-service.rc
+LOCAL_MODULE := android.hardware.power@1.2-service-qti
+LOCAL_INIT_RC := android.hardware.power@1.2-service-qti.rc
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_EXECUTABLE)
